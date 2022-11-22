0ddbedf02b29566acadb00ecce2b70a408282b12
migellebarlis <migellejosebarlis@gmail.com>
2022-11-22 18:47:00 +0800

    formatting

--- LuminanceFilter.py
+++ LuminanceFilter.py
@@ -1,12 +1,20 @@
+from typing import Any
+
 import tensorflow as tf
 import tensorflow_io as tfio
 from keras.layers import Layer
+from tensorflow import Tensor, IndexedSlices, SparseTensor
+
 
 class LuminanceFilter(Layer):
     def __init__(self, **kwargs):
         super(LuminanceFilter, self).__init__(**kwargs)
-   
+
     def call(self, input_data):
-        ycbcr = tfio.experimental.color.rgb_to_ycbcr(input_data)
-        y = ycbcr[:,:,0]
+        ycbcr = tfio.experimental.color.rgb_to_ycbcr(tf.cast(input_data, dtype=tf.uint8))
+        y = ycbcr[:, :, 0]
         return y
+
+
+if __name__ == '__main__':
+    print()
--- app.py
+++ app.py
@@ -4,80 +4,79 @@ from FFT2 import FFT2
 from LuminanceFilter import LuminanceFilter
 
 TRAINING_DIR = 'src/training'
-training_datagen = ImageDataGenerator(rescale = 1./255)
+training_datagen = ImageDataGenerator(rescale=1. / 255)
 train_generator = training_datagen.flow_from_directory(
-  TRAINING_DIR,
-  target_size = (800,800),
-  class_mode = 'categorical'
+    TRAINING_DIR,
+    target_size=(800, 800),
+    class_mode='categorical'
 )
 
 VALIDATION_DIR = 'src/validation'
-validation_datagen = ImageDataGenerator(rescale = 1./255)
+validation_datagen = ImageDataGenerator(rescale=1. / 255)
 validation_generator = validation_datagen.flow_from_directory(
-  VALIDATION_DIR,
-  target_size = (800,800),
-  class_mode = 'categorical'
+    VALIDATION_DIR,
+    target_size=(800, 800),
+    class_mode='categorical'
 )
 
 TESTING_DIR = 'src/testing'
-testing_datagen = ImageDataGenerator(rescale = 1./255)
+testing_datagen = ImageDataGenerator(rescale=1. / 255)
 testing_generator = testing_datagen.flow_from_directory(
-  TESTING_DIR,
-  target_size = (800,800),
-  class_mode = 'categorical'
+    TESTING_DIR,
+    target_size=(800, 800),
+    class_mode='categorical'
 )
 
 model = tf.keras.models.Sequential([
-  LuminanceFilter(),
-  tf.keras.layers.Conv2D(
-    filters = 64,
-    kernel_size = (3,3),
-    activation = None,
-    input_shape = (800,800)
-  ),
-  FFT2(),
-  tf.keras.layers.Conv2D(
-    filters = 64,
-    kernel_size = (3,3),
-    activation = 'relu',
-    input_shape = (800,800)
-  ),
-  tf.keras.layers.MaxPooling2D(2, 2),
-  tf.keras.layers.Conv2D(
-    filters = 64,
-    kernel_size = (3,3),
-    activation = 'relu'
-  ),
-  tf.keras.layers.MaxPooling2D(2, 2),
-  tf.keras.layers.Conv2D(
-    filters = 128,
-    kernel_size = (3,3),
-    activation = 'relu'
-  ),
-  tf.keras.layers.MaxPooling2D(2, 2),
-  tf.keras.layers.Conv2D(
-    filters = 128,
-    kernel_size = (3,3),
-    activation = 'relu'
-  ),
-  tf.keras.layers.MaxPooling2D(2, 2),
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dropout(0.5),
-  tf.keras.layers.Dense(512, activation='relu'),
-  tf.keras.layers.Dense(8, activation='softmax')
+    tf.keras.Input(shape=(800, 800, 3)),
+    LuminanceFilter(),
+    tf.keras.layers.Conv2D(
+        filters=64,
+        kernel_size=(3, 3),
+        activation=None
+    ),
+    FFT2(),
+    tf.keras.layers.Conv2D(
+        filters=64,
+        kernel_size=(3, 3),
+        activation='relu'
+    ),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(
+        filters=64,
+        kernel_size=(3, 3),
+        activation='relu'
+    ),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(
+        filters=128,
+        kernel_size=(3, 3),
+        activation='relu'
+    ),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(
+        filters=128,
+        kernel_size=(3, 3),
+        activation='relu'
+    ),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(8, activation='softmax')
 ])
 
-model.compile(optimizer = 'rmsprop',
-              loss = 'sparse_categorical_crossentropy',
-              metrics = ['accuracy'])
+model.compile(optimizer='rmsprop',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
 model.summary()
 
 model.fit(
-  train_generator,
-  epochs= 25,
-  validation_data = validation_generator,
-  verbose = 1)
+    train_generator,
+    epochs=25,
+    validation_data=validation_generator,
+    verbose=1)
 
 model.save('model/kernel_classifier')
 
