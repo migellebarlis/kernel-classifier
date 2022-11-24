2a27314ed8438f30b94bb85802101501d08e9b64
migellebarlis <migellejosebarlis@gmail.com>
2022-11-24 12:31:07 +0800

    fixed model

--- kernel_net.py
+++ kernel_net.py
@@ -1,4 +1,3 @@
-from random import shuffle
 import tensorflow as tf
 import pickle
 from FFT2 import FFT2
@@ -8,14 +7,12 @@ def parse_image(filename):
     # Remove the file extension
     file = tf.strings.split(filename, '.')[-2]
 
-    # Get the kernel number
-    kernel = tf.strings.to_number(tf.strings.split(file, '_')[-1])
+    # Get the zero-based kernel number
+    kernel = tf.strings.to_number(tf.strings.split(file, '_')[-1]) - 1
 
     # Load the image
     image = tf.io.read_file(filename)
     image = tf.io.decode_png(image)
-    image = tf.image.convert_image_dtype(image, tf.float32)
-    image = tf.squeeze(image)
 
     return image, kernel
 
@@ -33,31 +30,30 @@ valid_ds = valid_ds.batch(100)
 valid_ds = valid_ds.prefetch(1)
 
 # Define the shared CNN model
-shared_cnn = tf.keras.Sequential()
-shared_cnn.add(tf.keras.layers.Input(shape=(128, 128, 1), name='cnn_input'))
-shared_cnn.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation=None, padding='valid'))
-shared_cnn.add(FFT2())
-shared_cnn.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='valid'))
-shared_cnn.add(tf.keras.layers.MaxPool2D())
-shared_cnn.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='valid'))
-shared_cnn.add(tf.keras.layers.MaxPool2D())
-shared_cnn.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='valid'))
-shared_cnn.add(tf.keras.layers.MaxPool2D())
-shared_cnn.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='valid'))
-shared_cnn.add(tf.keras.layers.MaxPool2D())
-shared_cnn.add(tf.keras.layers.Flatten())
-shared_cnn.add(tf.keras.layers.Dropout(0.5))
-shared_cnn.add(tf.keras.layers.Dense(512, activation='relu'))
-shared_cnn.add(tf.keras.layers.Dense(8, activation='softmax'))
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Input(shape=(128, 128, 1), name='cnn_input'))
+model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation=None, padding='valid'))
+model.add(FFT2())
+model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='valid'))
+model.add(tf.keras.layers.MaxPool2D())
+model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='valid'))
+model.add(tf.keras.layers.MaxPool2D())
+model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='valid'))
+model.add(tf.keras.layers.MaxPool2D())
+model.add(tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='valid'))
+model.add(tf.keras.layers.MaxPool2D())
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(512, activation='relu'))
+model.add(tf.keras.layers.Dense(8, activation='softmax'))
 
 # Compile the model and perform the training
-model = tf.keras.models.Model(shared_cnn)
 model.compile(
     optimizer='adam',
     loss='sparse_categorical_crossentropy',
     metrics=['MeanAbsoluteError', 'MeanAbsolutePercentageError'])
+model.summary()
 
-print(model.summary())
 early_stop_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath='kernel_net', monitor='val_loss', save_best_only=True)
 history = model.fit(x=train_ds, batch_size=100, epochs=500, validation_data=valid_ds, callbacks=[early_stop_callback, model_checkpoint_callback])
